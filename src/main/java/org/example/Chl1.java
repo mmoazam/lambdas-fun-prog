6a0cac7b29e2d91e8879d40ca12ac668b90a704b
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-08 11:22:52 +0100

    Fix: chl1

--- src/main/java/org/example/Chl1.java
+++ src/main/java/org/example/Chl1.java
@@ -1,5 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 public class Chl1 {
+
+    public static void main(String[] args) {
+
+        String[] emails = new String[]{"simon.morgan@northcoders.com",
+                "chris.ward@northcoders.com",
+                "info@stackoverflow.com",
+                "hannah.montana@mileycyrus.com"};
+
+        Arrays.stream(emails).forEach(s -> printIndexOfAtChar.accept(s));
+        Stream.of(emails).forEach(s -> printIndexOfAtChar.accept(s));
+
+    } // end main
+
+    static Consumer<String> printIndexOfAtChar = s -> {
+            System.out.println(s.indexOf("@"));
+        };
 
 }
