57b099ff1371592e5590691633f474e1f3707623
nada <ainkharansoftdev@gmail.com>
2024-05-08 12:08:33 +0100

    Ch02

--- src/main/java/org/example/Chl1.java
+++ src/main/java/org/example/Chl1.java
@@ -8,17 +8,19 @@ import java.util.stream.Stream;
 
 public class Chl1 {
 
+    static String[] emails = new String[]{"simon.morgan@northcoders.com",
+            "chris.ward@northcoders.com",
+            "info@stackoverflow.com",
+            "hannah.montana@mileycyrus.com"};
+
     public static void main(String[] args) {
 
-        String[] emails = new String[]{"simon.morgan@northcoders.com",
-                "chris.ward@northcoders.com",
-                "info@stackoverflow.com",
-                "hannah.montana@mileycyrus.com"};
 
         Arrays.stream(emails).forEach(s -> printIndexOfAtChar.accept(s));
         Stream.of(emails).forEach(s -> printIndexOfAtChar.accept(s));
         // Ch02
-        System.out.println(getEmails.get());
+        //System.out.println(getEmails.get());
+        System.out.println(getEmails1.get());
 
     } // end main
 
@@ -26,9 +28,19 @@ public class Chl1 {
         System.out.println(s.indexOf("@"));
     };
     //Ch02 - Return email
-    static Supplier<List<String>> getEmails = () -> List.of(
-            "simon.morgan@northcoders.com",
-            "chris.ward@northcoders.com",
-            "info@stackoverflow.com",
-            "hannah.montana@mileycyrus");
+    static Supplier<List<String>> getEmails = () ->
+            List.of(
+                    "simon.morgan@northcoders.com",
+                    "chris.ward@northcoders.com",
+                    "info@stackoverflow.com",
+                    "hannah.montana@mileycyrus");
+
+    static Supplier<String> getEmails1 = () -> {
+        String str = "" ;
+        for (String s : emails) {
+            str =s ;
+        } return str;
+    };
+
+
 }
