0eddcc8c0ee7425ac4e196600cf55575a241c9b3
nada <ainkharansoftdev@gmail.com>
2024-05-08 11:35:50 +0100

    Ch02

--- src/main/java/org/example/Chl1.java
+++ src/main/java/org/example/Chl1.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Chl1 {
@@ -15,11 +17,18 @@ public class Chl1 {
 
         Arrays.stream(emails).forEach(s -> printIndexOfAtChar.accept(s));
         Stream.of(emails).forEach(s -> printIndexOfAtChar.accept(s));
+        // Ch02
+        System.out.println(getEmails.get());
 
     } // end main
 
     static Consumer<String> printIndexOfAtChar = s -> {
-            System.out.println(s.indexOf("@"));
-        };
-
+        System.out.println(s.indexOf("@"));
+    };
+    //Ch02 - Return email
+    static Supplier<List<String>> getEmails = () -> List.of(
+            "simon.morgan@northcoders.com",
+            "chris.ward@northcoders.com",
+            "info@stackoverflow.com",
+            "hannah.montana@mileycyrus");
 }
